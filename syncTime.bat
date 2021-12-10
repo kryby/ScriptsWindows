08498230c4f873cd7df9cabf246a4f4cd6850d73
Papá informático <info@papainformatico.es>
2021-12-10 11:14:18 +0100

    SyncTime
    Script para forzar la sincronización de un equipo windows con time.google.com

---
+++ syncTime.bat
@@ -0,0 +1,28 @@
+@echo off 
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+w32tm /config /syncfromflags:manual /manualpeerlist:"time.google.com" /reliable:YES /update
+w32tm /config /update
+w32tm /resync

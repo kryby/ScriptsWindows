e2a20faf689df3ca2102299216b4a976ea3bcfc2
Papá informático <info@papainformatico.es>
2021-12-15 13:14:56 +0100

    Update ConectOpenVpn.bat

--- ConectOpenVpn.bat
+++ ConectOpenVpn.bat
@@ -1,6 +1,6 @@
 +@echo off 
 "C:\Program Files\OpenVpn\bin\openvpn-gui.exe"
-SLEEP 10
+TIMEOUT /T 10
 "C:\Program Files\OpenVpn\bin\openvpn-gui.exe" --connect <nombre_archivo_configuracion_sin_extension>
-SLEEP 30
+TIMEOUT /T 30
 <Archivo a ejecutar despues de la conexión>

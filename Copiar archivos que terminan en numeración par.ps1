29bce18500c76c41b237cea35df5cb33765ba161
Papá informático <info@papainformatico.es>
2022-01-26 09:58:41 +0100

    Copiar archivos terminados en numero par

---
+++ Copiar archivos que terminan en numeración par.ps1
@@ -0,0 +1,4 @@
+﻿Get-ChildItem "Directorio origen" |
+Where-Object {$_.Name.EndsWith("0.pdf") -or $_.Name.EndsWith("2.pdf") -or $_.Name.EndsWith("4.pdf") -or $_.Name.EndsWith("6.pdf") -or $_.Name.EndsWith("8.pdf")} |
+Select -ExpandProperty FullName |
+Copy-Item -Destination Directorio destino

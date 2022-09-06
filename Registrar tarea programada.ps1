4b17d68b4d158debae30ad235c1d0110b84e0f6b
Papá informático <info@papainformatico.es>
2022-09-06 15:40:55 +0200

    Script create sheduled task

---
+++ Registrar tarea programada.ps1
@@ -0,0 +1,6 @@
+﻿$Argument = '' # Parametros del programa a ejecutar
+$Action = New-ScheduledTaskAction -Execute 'Path del programa a ejecutar' -Argument $Argument
+
+$Trigger = New-ScheduledTaskTrigger -Daily -At 5am
+
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Nombre tarea" -Description "Descripcion"

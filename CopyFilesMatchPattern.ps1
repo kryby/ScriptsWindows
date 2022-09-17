a2b66826a615c035be2b7ccd4dc978058d3a1817
Papá informático <info@papainformatico.es>
2022-09-17 09:45:02 +0200

    Create CopyFilesMatchPattern.ps1

---
+++ CopyFilesMatchPattern.ps1
@@ -0,0 +1,5 @@
+﻿$searchPath = '' #directory to search for files
+#Regex Numeric Range Generator -> https://3widgets.com/
+$RX = "(1220549[89]|122055[0-4][0-9]|1220555[0-6])" #match to look for
+$destinationFolder = '' #directory where to copy files matching the search pattern
+Get-ChildItem -Path $searchPath | Where-Object{$_.Name -match $RX} | Select Name, DirectoryName,FullName | Select -ExpandProperty FullName | Copy-Item -Destination $destinationFolder
